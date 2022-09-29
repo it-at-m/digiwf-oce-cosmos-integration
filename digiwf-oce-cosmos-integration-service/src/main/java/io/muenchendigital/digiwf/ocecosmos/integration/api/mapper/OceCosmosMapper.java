81c5f3f2c7d235d7d4f93ac78ac600bc06aa3a2c
darenegade <darenegade@gmx.de>
2022-09-29 13:37:56 +0200

    ignore file in mapping

--- digiwf-oce-cosmos-integration-service/src/main/java/io/muenchendigital/digiwf/ocecosmos/integration/api/mapper/OceCosmosMapper.java
+++ digiwf-oce-cosmos-integration-service/src/main/java/io/muenchendigital/digiwf/ocecosmos/integration/api/mapper/OceCosmosMapper.java
@@ -4,10 +4,12 @@ import io.muenchendigital.digiwf.ocecosmos.integration.api.configuration.Mapstru
 import io.muenchendigital.digiwf.ocecosmos.integration.api.dto.request.JobRequestDto;
 import io.muenchendigital.digiwf.ocecosmos.integration.model.request.JobRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapstructConfiguration.class)
 public interface OceCosmosMapper {
 
+    @Mapping(target = "file", ignore = true)
     JobRequest dto2Model(final JobRequestDto jobRequestDto);
 
 
