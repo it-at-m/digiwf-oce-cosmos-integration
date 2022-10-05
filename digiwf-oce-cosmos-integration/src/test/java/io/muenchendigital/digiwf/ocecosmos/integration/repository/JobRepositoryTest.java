0148c43baa3cc9755860f3aa523a138d7c62fb2e
darenegade <darenegade@gmx.de>
2022-10-05 12:07:26 +0200

    job Repo Tests

--- digiwf-oce-cosmos-integration/src/test/java/io/muenchendigital/digiwf/ocecosmos/integration/repository/JobRepositoryTest.java
+++ digiwf-oce-cosmos-integration/src/test/java/io/muenchendigital/digiwf/ocecosmos/integration/repository/JobRepositoryTest.java
@@ -6,6 +6,8 @@ import io.muenchendigital.digiwf.ocecosmos.integration.exception.OceCosmosIntegr
 import io.muenchendigital.digiwf.ocecosmos.integration.gen.api.FileStatusOperationsApi;
 import io.muenchendigital.digiwf.ocecosmos.integration.gen.api.JobAttributeOperationsApi;
 import io.muenchendigital.digiwf.ocecosmos.integration.gen.api.JobOperationsApi;
+import io.muenchendigital.digiwf.ocecosmos.integration.gen.model.Filestatus;
+import io.muenchendigital.digiwf.ocecosmos.integration.gen.model.JobAttribute;
 import io.muenchendigital.digiwf.ocecosmos.integration.gen.model.Jobs;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,13 +21,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -119,6 +125,39 @@ class JobRepositoryTest {
     }
 
     @Test
+    void queryOneJob() throws OceCosmosIntegrationException, OceCosmosIntegrationServerErrorException, OceCosmosIntegrationClientErrorException {
+        final var jobRequest = getPrefilledJob();
+        final var jobsReturn = Collections.singletonList(new Jobs().parentId(123L));
+
+        String search = String.format("status==0 and customField==SAMMELJOB and customField1==%s and customField2==%s",
+                jobRequest.getCustomField1(), jobRequest.getCustomField2());
+        Mockito.when(this.jobOperationsApi.getUsingGET7(null, null, null, search)).thenReturn(jobsReturn);
+
+        final var result = this.jobRepository.queryJobs(jobRequest);
+
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0).getParentId(), is(123L));
+
+        Mockito.verify(this.jobOperationsApi, Mockito.times(1)).getUsingGET7(null, null, null, search);
+    }
+
+    @Test
+    void queryJobMultipleJobs() throws OceCosmosIntegrationException, OceCosmosIntegrationServerErrorException, OceCosmosIntegrationClientErrorException {
+        final var jobRequest = getPrefilledJob();
+        final var jobsReturn = Arrays.asList(new Jobs().parentId(123L), new Jobs().parentId(456L));
+
+        String search = String.format("status==0 and customField==SAMMELJOB and customField1==%s and customField2==%s",
+                jobRequest.getCustomField1(), jobRequest.getCustomField2());
+        Mockito.when(this.jobOperationsApi.getUsingGET7(null, null, null, search)).thenReturn(jobsReturn);
+
+        final var result = this.jobRepository.queryJobs(jobRequest);
+
+        assertThat(result.size(), is(2));
+
+        Mockito.verify(this.jobOperationsApi, Mockito.times(1)).getUsingGET7(null, null, null, search);
+    }
+
+    @Test
     void queryJobClientException() {
         final var jobRequest = getPrefilledJob();
 
@@ -160,6 +199,112 @@ class JobRepositoryTest {
         Mockito.verify(this.jobOperationsApi, Mockito.times(1)).getUsingGET7(any(), any(), any(), any());
     }
 
+    @Test
+    void addJobAttributesToJob() throws OceCosmosIntegrationException, OceCosmosIntegrationServerErrorException, OceCosmosIntegrationClientErrorException {
+        final var attributes = getPrefilledJobAttributes();
+
+        this.jobRepository.addJobAttributesToJob(attributes, 123L);
+
+        Mockito.verify(this.jobAttributeOperationsApi, Mockito.times(1)).putUsingPUT2(attributes, 123L);
+    }
+
+    @Test
+    void addJobAttributesToJobClientException() {
+        final var attributes = getPrefilledJobAttributes();
+
+        doThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST)).when(this.jobAttributeOperationsApi).putUsingPUT2(any(), any());
+
+        Exception exception = assertThrows(OceCosmosIntegrationClientErrorException.class,
+                () -> this.jobRepository.addJobAttributesToJob(attributes, 123L));
+
+        assertThat(exception.getMessage(), is("The request to add Jobattributes to a Job failed with 400 BAD_REQUEST."));
+
+        Mockito.verify(this.jobAttributeOperationsApi, Mockito.times(1)).putUsingPUT2(any(), any());
+    }
+
+    @Test
+    void addJobAttributesToJobServerException() {
+        final var attributes = getPrefilledJobAttributes();
+
+        doThrow(new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR)).when(this.jobAttributeOperationsApi).putUsingPUT2(any(), any());
+
+        Exception exception = assertThrows(OceCosmosIntegrationServerErrorException.class,
+                () -> this.jobRepository.addJobAttributesToJob(attributes, 123L));
+
+        assertThat(exception.getMessage(), is("The request to add Jobattributes to a Job failed with 500 INTERNAL_SERVER_ERROR."));
+
+        Mockito.verify(this.jobAttributeOperationsApi, Mockito.times(1)).putUsingPUT2(any(), any());
+    }
+
+    @Test
+    void addJobAttributesToJobUnknownException() {
+        final var attributes = getPrefilledJobAttributes();
+
+        doThrow(new RuntimeException("Look at me, I'm an exception")).when(this.jobAttributeOperationsApi).putUsingPUT2(any(), any());
+
+        Exception exception = assertThrows(OceCosmosIntegrationException.class,
+                () -> this.jobRepository.addJobAttributesToJob(attributes, 123L));
+
+        assertThat(exception.getMessage(), is("The request to add Jobattributes to a Job failed."));
+
+        Mockito.verify(this.jobAttributeOperationsApi, Mockito.times(1)).putUsingPUT2(any(), any());
+    }
+
+    @Test
+    void addDataToJob() throws OceCosmosIntegrationException, OceCosmosIntegrationServerErrorException, OceCosmosIntegrationClientErrorException {
+        final var status = getPrefilledFilestatus();
+        final var file = Path.of("tmp/test.json");
+
+        this.jobRepository.addDataToJob(status, file, 123L);
+
+        Mockito.verify(this.fileStatusOperationsApi, Mockito.times(1)).postUsingPOST2(123L, file, status);
+    }
+
+    @Test
+    void addDataToJobClientException() {
+        final var status = getPrefilledFilestatus();
+        final var file = Path.of("tmp/test.json");
+
+        doThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST)).when(this.fileStatusOperationsApi).postUsingPOST2(any(), any(), any());
+
+        Exception exception = assertThrows(OceCosmosIntegrationClientErrorException.class,
+                () -> this.jobRepository.addDataToJob(status, file, 123L));
+
+        assertThat(exception.getMessage(), is("The request to add Data to a Job failed with 400 BAD_REQUEST."));
+
+        Mockito.verify(this.fileStatusOperationsApi, Mockito.times(1)).postUsingPOST2(123L, file, status);
+    }
+
+    @Test
+    void addDataToJobServerException() {
+        final var status = getPrefilledFilestatus();
+        final var file = Path.of("tmp/test.json");
+
+        doThrow(new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR)).when(this.fileStatusOperationsApi).postUsingPOST2(any(), any(), any());
+
+        Exception exception = assertThrows(OceCosmosIntegrationServerErrorException.class,
+                () -> this.jobRepository.addDataToJob(status, file, 123L));
+
+        assertThat(exception.getMessage(), is("The request to add Data to a Job failed with 500 INTERNAL_SERVER_ERROR."));
+
+        Mockito.verify(this.fileStatusOperationsApi, Mockito.times(1)).postUsingPOST2(123L, file, status);
+    }
+
+    @Test
+    void addDataToJobUnknownException() {
+        final var status = getPrefilledFilestatus();
+        final var file = Path.of("tmp/test.json");
+
+        doThrow(new RuntimeException("Look at me, I'm an exception")).when(this.fileStatusOperationsApi).postUsingPOST2(any(), any(), any());
+
+        Exception exception = assertThrows(OceCosmosIntegrationException.class,
+                () -> this.jobRepository.addDataToJob(status, file, 123L));
+
+        assertThat(exception.getMessage(), is("The request to add Data to a Job failed."));
+
+        Mockito.verify(this.fileStatusOperationsApi, Mockito.times(1)).postUsingPOST2(123L, file, status);
+    }
+
     private Jobs getPrefilledJob() {
         return new Jobs()
                 .client("Test")
@@ -169,6 +314,24 @@ class JobRepositoryTest {
                 .inputChannel("RESTAPI")
                 .customField("SAMMELJOB")
                 .customField1("Test");
+    }
+
+    private List<JobAttribute> getPrefilledJobAttributes() {
+        return Arrays.asList(
+                new JobAttribute().jobId(123L).attributeName("Attribute1").attributeValue("Attribute1Value"),
+                new JobAttribute().jobId(123L).attributeName("Attribute2").attributeValue("Attribute2Value")
+        );
+
+    }
+
+    private Filestatus getPrefilledFilestatus() {
+        return new Filestatus()
+                .jobId(123L)
+                .textStatus("Hochgeladen XYZ")
+                .fileName("Test.json")
+                .application("OceInt")
+                .fileType("INPUT")
+                .dataType("JSON");
     }
 
 }
